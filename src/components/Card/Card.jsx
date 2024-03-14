d042950c79a1706baddc8a1d9c8db7e598d90b97
Lucas Cubile <cubilelucas@gmail.com>
2024-03-14 13:34:45 -0300

    chore: Translate Cell to English name

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ id, image, name, race, ki, handleClick, shuffle }) => {
       <div className='card-image-container'>
         <img src={image} alt={name} loading='lazy' />
       </div>
-      <h3>{name}</h3>
+      <h3>{name === 'Celula' ? 'Cell' : name}</h3>
       <div className='char-info'>
         <p>Race: {race}</p>
         <p>Ki: {ki}</p>
