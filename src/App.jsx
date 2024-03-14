823bca6d19f025385c95a5998461c83c898e686a
Lucas Cubile <cubilelucas@gmail.com>
2024-03-14 12:18:31 -0300

    feat: :sparkles: Add a welcome modal to explain the game

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Main from './layout/Main/Main';
 import Aside from './layout/Aside/Aside';
 import Footer from './layout/Footer/Footer';
 import ResetModal from './components/ResetModal/ResetModal';
+import WelcomeModal from './components/WelcomeModal/WelcomeModal';
 
 function App() {
   const [clickedCards, setClickedCards] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
   const [best, setBest] = useState(0);
   const [isGameOver, setIsGameOver] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
 
   const handleClick = (id) => {
     if (!clickedCards.includes(id)) {
@@ -29,7 +31,8 @@ function App() {
   return (
     <>
       <Header score={currentScore} best={best} />
-      {!isGameOver && <Main handleClick={handleClick} />}
+      {!isStarted && <WelcomeModal setIsStarted={setIsStarted} />}
+      {!isGameOver && isStarted && <Main handleClick={handleClick} />}
       {isGameOver && (
         <ResetModal
           setClickedCards={setClickedCards}
---
+++ src/components/WelcomeModal/WelcomeModal.jsx
@@ -0,0 +1,17 @@
+import './WelcomeModal.scss';
+
+const WelcomeModal = ({ setIsStarted }) => {
+  return (
+    <div className='welcome-modal'>
+      <p>Help goku get enough energy for his spirit bomb</p>
+      <p>
+        Click on each character only once. If you click a previously clicked
+        card, Goku will lose the gathered energy.
+      </p>
+      <button type='button' onClick={() => setIsStarted(true)}>
+        Start
+      </button>
+    </div>
+  );
+};
+export default WelcomeModal;
